e9b07a4b1cecd74be6f76aeea1d179f30346cc79
Thomas Servuc <thomas.selle@orange.fr>
2015-12-01 15:12:40 +0100

    Correction

--- test/index.js
+++ test/index.js
@@ -3,6 +3,6 @@ import shapUc from '../lib';
 
 describe('shap-uc', function () {
   it('should have unit test!', function () {
-    assert(typeof shapes.VERSION !== 'undefined', 'The Project should have a VERSION, whatever the actual version.');
+    assert(typeof shapUc.VERSION !== 'undefined', 'The Project should have a VERSION, whatever the actual version.');
   });
 });
